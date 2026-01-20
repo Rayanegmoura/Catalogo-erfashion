b1256cc2e0c874a43ccce07f12ccd32a00b01948
rayanegmoura <rayaneg_moura@hotmail.com>
2026-01-19 23:36:00 -0300

    pg contato completa

--- js/script.js
+++ js/script.js
@@ -46,4 +46,37 @@ document.querySelectorAll('.tela-produto').forEach(produto => {
   });
 });
 
+// MULTIPLAS IMAGENS - SLIDER LOJA FISICA
+
+document.querySelectorAll('.loja-fisica').forEach(produto => {
+  const imgs = produto.querySelectorAll('.slider-produto img');
+  const dots = produto.querySelectorAll('.slider-dots .dot');
+  let index = 0;
+
+  const showImg = i => {
+    imgs.forEach(img => img.classList.remove('active'));
+    dots.forEach(dot => dot.classList.remove('active'));
+    imgs[i].classList.add('active');
+    dots[i].classList.add('active');
+  };
+
+  // clicar na imagem para avançar
+  produto.querySelector('.slider-produto').addEventListener('click', () => {
+    index = (index + 1) % imgs.length;
+    showImg(index);
+  });
+
+  // clicar nos dots
+  dots.forEach((dot, i) => {
+    dot.addEventListener('click', () => {
+      index = i;
+      showImg(index);
+    });
+  });
+});
+
+
+
+
+
 
